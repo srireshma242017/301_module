bd8265092f260a0a459032cf724881a439d951fd
srireshma242017 <srireshma2003@gmail.com>
2023-01-15 18:33:22 +0530

    installed minimist and updated it

--- level_2/index.js
+++ level_2/index.js
@@ -1,49 +1,49 @@
 const http = require("http");
 const fs = require("fs");
-
+const args = require("minimist")(process.argv.slice(1));
 
 let homeContent = "";
 let projectContent = "";
 let regContent = "";
 
 fs.readFile("home.html", (err, home) => {
-  if (err) {
-    throw err;
-  }
-  homeContent = home;
+    if (err) {
+        throw err;
+    }
+    homeContent = home;
 });
 
 fs.readFile("registration.html", (err, registration) => {
     if (err) {
-      throw err;
+        throw err;
     }
     regContent = registration;
-  });
+});
 
 fs.readFile("project.html", (err, project) => {
-  if (err) {
-    throw err;
-  }
-  projectContent = project;
+    if (err) {
+        throw err;
+    }
+    projectContent = project;
 });
 
 http
-  .createServer((request, response) => {
-    let url = request.url;
-    response.writeHeader(200, { "Content-Type": "text/html" });
-    switch (url) {
-      case "/project":
-        response.write(projectContent);
-        response.end();
-        break;
-      case "/registration":
-        response.write(regContent);
-        response.end();
-        break;
-      default:
-        response.write(homeContent);
-        response.end();
-        break;
-    }
-  })
-  .listen(5000);
+    .createServer((request, response) => {
+        let url = request.url;
+        response.writeHeader(200, { "Content-Type": "text/html" });
+        switch (url) {
+            case "/project":
+                response.write(projectContent);
+                response.end();
+                break;
+            case "/registration":
+                response.write(regContent);
+                response.end();
+                break;
+            default:
+                response.write(homeContent);
+                response.end();
+                break;
+        }
+    })
+    .listen(args.port);
