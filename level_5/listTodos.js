70833a35a22cc37cbd6640036490c3ed9f6992aa
srireshma242017 <srireshma2003@gmail.com>
2023-01-20 22:40:03 +0530

    added code for listing the todolist

--- level_5/listTodos.js
+++ level_5/listTodos.js
@@ -1,7 +1,5 @@
-//  listTodos.js
-var db = require("./models/index");
-
-var listTodo = async () => {
+const db = require("./models/index")
+const listTodo = async () => {
     try {
         await db.Todo.showList();
     } catch (error) {
